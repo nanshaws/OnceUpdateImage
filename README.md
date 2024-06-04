b00e4cf20869add1ea4816f28ca1145ba2556d32
cover <98329722+nanshaws@users.noreply.github.com>
2024-06-05 07:28:34 +0800

    update 1.2
    update 1.2

--- README.md
+++ README.md
@@ -13,5 +13,17 @@ magick convert player.png -crop 48x48 player/frame_%02d.png
 
 这条命令是将player.png文件直接以48x48分到player文件夹里面，以frame_为前缀，后面序号0-max.png
 
+![image-20240605071730961](./img/image-20240605071730961.png)
+
+![image-20240605071802932](./img/image-20240605071802932.png)
+
 这个时候使用此应用
+
+该项目代码已经打包成exe文件了。
+
+![image-20240605072655776](./img/image-20240605072655776.png)
+
+![image-20240605071506699](./img/image-20240605071506699.png)
+
+![image-20240605071641210](./img/image-20240605071641210.png)
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,34 +5,98 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入文件夹的名字:");
-        String directoryPath = scanner.nextLine(); // 用户输入的文件夹路径
+        System.out.println("功能如下。");
+        System.out.println("1、单纯修改部分前缀，2、根据序列号修改名字");
 
-        System.out.println("请输入旧的前缀:");
-        String oldPrefix = scanner.nextLine();
-        System.out.println("旧的前缀:"+oldPrefix);
-        System.out.println("请输入新的前缀:");
-        String newPrefix = scanner.nextLine();
-        System.out.println("新的前缀:"+newPrefix);
-        File dir = new File(directoryPath);
+        int select = scanner.nextInt();
+        scanner.nextLine(); // 消耗掉nextInt后的换行符
+        if (select == 1) {
+            System.out.println("请输入文件夹的名字:");
+            String directoryPath = scanner.nextLine(); // 用户输入的文件夹路径
 
-        if (dir.isDirectory()) {
-            File[] files = dir.listFiles();
-            for (File file : files) {
-                if (file.getName().startsWith(oldPrefix)){
-                    String newName = file.getName().replaceFirst(oldPrefix, newPrefix);
-                    File newFile = new File(dir, newName);
-                    if (file.renameTo(newFile)) {
-                        System.out.println("重命名: " + file.getName() + " -> " + newName);
-                    } else {
-                        System.out.println("重命名失败: " + file.getName());
+            System.out.println("请输入旧的前缀:");
+            String oldPrefix = scanner.nextLine();
+            System.out.println("旧的前缀:" + oldPrefix);
+            System.out.println("请输入新的前缀:");
+            String newPrefix = scanner.nextLine();
+            System.out.println("新的前缀:" + newPrefix);
+            File dir = new File(directoryPath);
+
+            if (dir.isDirectory()) {
+                File[] files = dir.listFiles();
+                for (File file : files) {
+                    if (file.getName().startsWith(oldPrefix)) {
+                        String newName = file.getName().replaceFirst(oldPrefix, newPrefix);
+                        File newFile = new File(dir, newName);
+                        if (file.renameTo(newFile)) {
+                            System.out.println("重命名: " + file.getName() + " -> " + newName);
+                        } else {
+                            System.out.println("重命名失败: " + file.getName());
+                        }
                     }
                 }
+            } else {
+                System.out.println(directoryPath + " 不是一个目录。");
             }
-        } else {
-            System.out.println(directoryPath + " 不是一个目录。");
+            scanner.close();
+        }else if (select==2){
+            System.out.println("请输入文件夹的名字:");
+            String directoryPath = scanner.nextLine(); // 用户输入的文件夹路径
+
+            System.out.println("请输入要修改的个数:");
+            int count = scanner.nextInt();
+            scanner.nextLine(); // 添加这行来消耗换行符
+
+            System.out.println("请依次输入要改的序列号:");
+            Integer[] s = new Integer[count];
+            for (int i = 0; i < count; i++) {
+                s[i] = scanner.nextInt();
+            }
+            scanner.nextLine(); // 添加这行来消耗换行符
+            System.out.println("请输入新的名字:");
+            String newName = scanner.nextLine();
+            String Name=newName;
+            System.out.println("新的名字:" + newName);
+            File dir = new File(directoryPath);
+
+            if (dir.isDirectory()) {
+                File[] files = dir.listFiles();
+                for (File file : files) {
+
+                    for (int i=0;i<count;i++){
+                        if (s[i]<10){
+                            if (file.getName().endsWith("0"+s[i]+".png")){
+                                newName=newName+"_"+s[i].toString()+".png";
+                                File newFile = new File(dir, newName);
+                                if (file.renameTo(newFile)) {
+                                System.out.println("重命名: " + file.getName() + " -> " + newName);
+                                 newName=Name;
+                                 } else {
+                                System.out.println("重命名失败: " + file.getName());
+                               }
+                            }
+                        }else {
+                            if (file.getName().endsWith(s[i]+".png")){
+                                newName=newName+"_"+s[i].toString()+".png";
+                                File newFile = new File(dir, newName);
+                                if (file.renameTo(newFile)) {
+                                    System.out.println("重命名: " + file.getName() + " -> " + newName);
+                                    newName=Name;
+                                } else {
+                                    System.out.println("重命名失败: " + file.getName());
+                                }
+                            }
+                        }
+
+                    }
+                }
+            } else {
+                System.out.println(directoryPath + " 不是一个目录。");
+            }
+            scanner.close();
+
         }
-        scanner.close();
     }
 }
